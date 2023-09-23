5f6272460a220fcdcdeba099c5c72e6cb1769745
Tahshon Holmes <95249900+TahshonHolmes@users.noreply.github.com>
2023-09-23 14:40:17 -0400

    Update product.py

--- product.py
+++ product.py
@@ -32,4 +32,4 @@ def add_product():
     return jsonify(new_product), 201
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
